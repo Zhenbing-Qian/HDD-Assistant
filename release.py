2f1271cd9b58b7e06465af3a5a6c9a8436e72536
Zhenbing-Qian <zhenbing.q.qian@gmail.com>
2016-03-04 15:21:34 +0800

    Added files via upload
    create release.py

---
+++ release.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#-----------------------------------------------------------------------------------------#
+# Description: Tool for HDD
+# $Id: //HDD Assistant/release.py $
+# $Date: 2016/02/29 $
+# $Author: zhenbing.q.qian $
+# $Source:$
+# Level: X
+#-----------------------------------------------------------------------------------------#
+
+TITLE     = "HDD Assistant"
+VERSION   = "0.1"
